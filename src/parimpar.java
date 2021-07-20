5e16786754980c3ead1351e36248ecccb37db122
paolaya12 <angieolaya1212@gmail.com>
2021-07-19 20:53:05 -0500

    commit inicial

---
+++ src/parimpar.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class parimpar {
+    public static void main(String [] args){
+        var sc = new Scanner(System.in);
+        System.out.println("Ingrese un número entero");
+        var numero = sc.nextInt();
+
+        var respuesta = validarNumero(numero);
+
+        System.out.println(respuesta);
+    }
+    public static String validarNumero(int numero){
+        return (numero%2 == 0 ? numero + " Es par" : numero + " Es impar");
+    }
+}
